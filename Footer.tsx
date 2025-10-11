e3fb4888423710de060c96cf9722837985b131d8
Minjin-Son <edugene47@gmail.com>
2025-10-11 21:52:27 +0900

    Add files via upload

--- Footer.tsx
+++ Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
         
         {/* 👇 이 부분을 추가하세요! */}
         <p className="mt-2"> 
-          기능 개선 사항이나 사이트 관련해서 문의 사항이 있으면 사이트 개발자 및 운영자 이메일 주소 : edugene47@gmail.com으로 보내주세요.
+          기능 개선 사항이나 사이트 관련해서 문의 사항이 있으면 사이트 개발자 및 운영자 이메일 주소인 edugene47@gmail.com으로 보내주세요.
         </p>
       </div>
     </footer>
